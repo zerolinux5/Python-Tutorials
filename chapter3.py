7ee434f7fa28e06377e1eecd311c4e5b29f14a81
Jesus Magana <maganazero5@gmail.com>
2014-08-06 14:42:19 -0700

    Solve missing file issue using try exception block

--- chapter3.py
+++ chapter3.py
@@ -44,4 +44,18 @@ if os.path.exists('sketch.txt'):
 else:
 	print('The data file is missing!')
 
+try:
+	for each_line in data:
+		try:
+			(role, line_spoken) = each_line.split(':', 1)
+			print(role, end='')
+			print(' said ', end='')
+			print(line_spoken, end='')
+		except:
+			pass
+
+	data.close()
+except:
+	print('The data file is missing!')
+
 
