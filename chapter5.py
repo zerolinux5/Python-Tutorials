09e6fb5338e8d9ae5accd91aa9945fe8e12fbc36
Jesus Magana <maganazero5@gmail.com>
2014-08-06 16:02:29 -0700

    Code that does list comprehension

--- chapter5.py
+++ chapter5.py
@@ -50,3 +50,28 @@ print(sorted(james2))
 print(sorted(julie2))
 print(sorted(mikey2))
 print(sorted(sarah2))
+
+
+mins = [1, 2, 3]
+secs = [m*60 for m in mins]
+print(secs)
+
+meters = [1, 10, 3]
+feet = [m * 3.281 for m in meters]
+print(feet)
+
+lower = ["I", "don't", "like", "spam"]
+upper = [s.upper() for s in lower]
+print(upper)
+
+dirty = ['2-22', '2:22', '2.22']
+clean = [sanitize(t) for t in dirty]
+print(clean)
+
+clean = [float(s) for s in clean]
+print(clean)
+
+clean = [float(sanitize(t)) for t in ['2-22', '3:33', '4.44']]
+print(clean)
+
+
