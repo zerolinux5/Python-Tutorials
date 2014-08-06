221419db89db1665b3485059b2775eda06a847f9
Jesus Magana <maganazero5@gmail.com>
2014-08-06 14:03:57 -0700

    Updated print to tab

---
+++ nester.py
@@ -0,0 +1,18 @@
+""" This .py file gives a function called print_lol
+    which prints all the nested lists of a list
+    Arguments: a list
+    Side Effects: prints to screen
+"""
+
+def print_lol(the_list, level):
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item, level+1)
+		else:
+			for tab in range(level):
+				print("\t", end=")
+			print(each_item)
+
+movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91, ["Graham Chapman", ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]
+
+print_lol(movies, 0)
