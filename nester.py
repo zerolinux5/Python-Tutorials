b49063aa9d7ed34c42c4bcaeb7aad67551c24272
Jesus Magana <maganazero5@gmail.com>
2014-08-06 15:08:45 -0700

    Modded nester.py to handle file output

--- nester.py
+++ nester.py
@@ -3,13 +3,14 @@
     Arguments: a list
     Side Effects: prints to screen
 """
+import sys
 
-def print_lol(the_list, indent = False,  level = 0):
+def print_lol(the_list, indent = False,  level = 0, fh=sys.stdout):
 	for each_item in the_list:
 		if isinstance(each_item, list):
-			print_lol(each_item, indent, level+1)
+			print_lol(each_item, indent, level+1, fh)
 		else:
 			if indent:
 				for tabs in range(level):
-					print("\t", end='')
-			print(each_item)
+					print("\t", end='', file=fh)
+			print(each_item, file=fh)
