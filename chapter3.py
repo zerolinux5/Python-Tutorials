8bf88e38796c388d81cd5593633f9a9248d71529
Jesus Magana <maganazero5@gmail.com>
2014-08-06 14:32:34 -0700

    use split function and find

--- chapter3.py
+++ chapter3.py
@@ -9,3 +9,12 @@ data.seek(0)
 for each_line in data:
 	print(each_line, end='')
 data.close()
+
+data = open('sketch.txt')
+for each_line in data:
+	if not each_line.find(':') == -1:
+		(role, line_spoken) = each_line.split(':', 1)
+		print(role, end='')
+		print(' said: ', end='')
+		print(line_spoken, end='')
+data.close()
