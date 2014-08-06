58a22106073608e4a83996f7879d2d1e530ebdc1
Jesus Magana <maganazero5@gmail.com>
2014-08-06 14:37:42 -0700

    Added code with try exception

--- chapter3.py
+++ chapter3.py
@@ -18,3 +18,16 @@ for each_line in data:
 		print(' said: ', end='')
 		print(line_spoken, end='')
 data.close()
+
+data = open('sketch.txt')
+
+for each_line in data:
+	try:
+		(role, line_spoken) = each_line.split(':', 1)
+		print(role, end='')
+		print(' said ', end='')
+		print(line_spoken, end='')
+	except:
+		pass
+
+data.close()
