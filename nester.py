efade81ae4664a3953f0b27922a68b6711441b34
Jesus Magana <maganazero5@gmail.com>
2014-08-06 14:11:08 -0700

    Have default value, note uses python3

--- nester.py
+++ nester.py
@@ -4,15 +4,15 @@
     Side Effects: prints to screen
 """
 
-def print_lol(the_list, level):
+def print_lol(the_list, level = 0):
 	for each_item in the_list:
 		if isinstance(each_item, list):
 			print_lol(each_item, level+1)
 		else:
-			for tab in range(level):
-				print("\t", end=")
+			for tabs in range(level):
+				print("\t", end='')
 			print(each_item)
 
-movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91, ["Graham Chapman", ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]
+movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91, ["Graham Chapman", ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
 
 print_lol(movies, 0)
