5b6f8f5c130d15f7d96fee981b0214507bab9cae
Jesus Magana <maganazero5@gmail.com>
2014-08-07 14:56:00 -0700

    Added json python tutorial

---
+++ jsonPractice.py
@@ -0,0 +1,9 @@
+import json
+names = ['John', ['Johnny', 'Jack'], 'Michael', ['Mike', 'Mikey', 'Mick']]
+print(names)
+
+to_transfer = json.dumps(names)
+print(to_transfer)
+from_transfer = json.loads(to_transfer)
+print(from_transfer)
+print(names)
