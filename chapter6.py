8fe3fd80b1afa547ec434a99406db8f3a2912e2b
Jesus Magana <maganazero5@gmail.com>
2014-08-07 09:08:43 -0700

    Executed dictionary exercises

--- chapter6.py
+++ chapter6.py
@@ -22,3 +22,21 @@ sarah = get_coach_data('sarah2.txt')
 (sarah_name, sarah_dob) = sarah.pop(0), sarah.pop(0)
 print(sarah_name+"'s fastest times are: "+ str(sorted(set([sanitize(t) for t in sarah]))[0:3]))
 
+cleese = {}
+palin = dict()
+print(type(cleese))
+print(type(palin))
+
+cleese['Name'] = 'John Cleese'
+cleese['Occupations'] = ['actor', 'comedian', 'writer', 'film producer']
+palin = {'Name' : 'Michael Palin', 'Occupations': ['comedian', 'actor', 'writer', 'tv']}
+
+print(palin['Name'])
+print(cleese['Occupations'][-1])
+
+palin['Birthplace'] = "Broomhill, Sheffield, England"
+cleese['Birthplace'] = "Weston-super-Mare, North Somerset, England"
+
+print(palin)
+
+print(cleese)
