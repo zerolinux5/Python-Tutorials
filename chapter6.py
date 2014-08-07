718b55338b3e7fc9f55a6fec4ab58ef083d1684d
Jesus Magana <maganazero5@gmail.com>
2014-08-07 10:07:29 -0700

    Athlete class is now inherited class

--- chapter6.py
+++ chapter6.py
@@ -117,3 +117,17 @@ print(johnny.name)
 for attr in johnny:
 	print(johnny.name + " is a " + attr + ".")
 
+class AthleteList(list):
+	def __init__(self, a_name, a_dob=None, a_times=[]):
+		list.__init__([])
+		self.name = a_name
+		self.dob = a_dob
+		self.extend(a_times)
+	def top3(self):
+		return(sorted(set([sanitize(t) for t in self]))[0:3])
+
+vera = AthleteList('Vera Vi')
+vera.append('1.31')
+print(vera.top3())
+vera.extend(['2-22', "1-21", '2:22'])
+print(vera.top3())
