9a229e200ad4140f010cf5fed98e840d886b2190
Jesus Magana <maganazero5@gmail.com>
2014-08-07 11:33:46 -0700

    Changed top3 to a property instead of a method

--- webapp/cgi-bin/athletelist.py
+++ webapp/cgi-bin/athletelist.py
@@ -1,18 +1,26 @@
 class AthleteList(list):
-        def __init__(self, a_name, a_dob=None, a_times=[]):
-                list.__init__([])
-                self.name = a_name
-                self.dob = a_dob
-                self.extend(a_times)
-        def top3(self):
-                return(sorted(set([sanitize(t) for t in self]))[0:3])
 
-def sanitize(times):
-        if '-' in times:
-                splitter = '-'
-        elif ':' in times:
-                splitter = ':'
+    def __init__(self, a_name, a_dob=None, a_times=[]):
+        list.__init__([])
+        self.name = a_name
+        self.dob = a_dob
+        self.extend(a_times)
+
+    @staticmethod
+    def sanitize(time_string):
+        if '-' in time_string:
+            splitter = '-'
+        elif ':' in time_string:
+            splitter = ':'
         else:
-                return(times)
-        (mins, secs) = times.split(splitter)
+            return(time_string)
+        (mins, secs) = time_string.split(splitter)
         return(mins + '.' + secs)
+
+    @property
+    def top3(self):
+        return(sorted(set([self.sanitize(t) for t in self]))[0:3])
+
+    @property
+    def clean_data(self):
+        return(sorted(set([self.sanitize(t) for t in self])))
--- webapp/cgi-bin/generate_timing_data.py
+++ webapp/cgi-bin/generate_timing_data.py
@@ -3,6 +3,8 @@
 import cgi
 import athletemodel
 import yate
+import cgitb
+cgitb.enable()
 
 athletes = athletemodel.get_from_store()
 
@@ -13,5 +15,5 @@ print(yate.start_response())
 print(yate.include_header("Coach Kelly's Timing Data"))
 print(yate.header("Athlete: " + athlete_name + ",DOB:" + athletes[athlete_name].dob + "."))
 print(yate.para("The top times for this athlete are:"))
-print(yate.u_list(athletes[athlete_name].top3()))
+print(yate.u_list(athletes[athlete_name].top3))
 print(yate.include_footer({"Home":"/index.html", "Select another athlete":"generate_list.py"}))
