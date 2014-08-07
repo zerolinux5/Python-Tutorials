f9192b13541244e92b1e3d3ca18be1f2571e8fb6
Jesus Magana <maganazero5@gmail.com>
2014-08-07 09:57:23 -0700

    added more methods to class

--- chapter6.py
+++ chapter6.py
@@ -5,6 +5,10 @@ class Athlete:
 		self.times = a_times
 	def top3(self):
 		return(sorted(set([sanitize(t) for t in self.times]))[0:3])
+	def add_time(self, time_value):
+		self.times.append(time_value)
+	def add_times(self, list_of_times):
+		self.times.extend(list_of_times)
 
 def get_coach_data(filename):
 	try:
@@ -92,3 +96,9 @@ print(james.name + "'s fastest times are: " + str(james.top3()))
 print(julie.name + "'s fastest times are: " + str(julie.top3()))
 print(mikey.name + "'s fastest times are: " + str(mikey.top3()))
 print(sarah.name + "'s fastest times are: " + str(sarah.top3()))
+
+vera = Athlete('Vera Vi')
+vera.add_time('1.31')
+print(vera.top3())
+vera.add_times(['2-22', "1-21", '2:22'])
+print(vera.top3())
